9ee1e3ec8bec86f8e7d129b567917c18fb89ba08
Antoine Brique <antoine.brique@gmail.com>
2022-03-17 08:31:41 +0100

    :art: Add instructions in README and implement all gems in minimal.rb

--- minimal.rb
+++ minimal.rb
@@ -5,6 +5,10 @@
 ########################################
 inject_into_file 'Gemfile', before: 'group :development, :test do' do
   <<~RUBY
+    gem 'devise'
+    gem 'stimulus-rails'
+    gem 'cssbundling-rails'
+    gem 'tailwindcss-rails', '~> 2.0'
     gem 'simple_form', github: 'heartcombo/simple_form'
   RUBY
 end
@@ -16,3 +20,17 @@ inject_into_file 'Gemfile', after: 'group :development, :test do' do
   gem 'dotenv-rails'
   RUBY
 end
+
+gsub_file('Gemfile', / gem 'sassc-rails'/, "# gem 'sassc-rails'")
+
+# Assets
+########################################
+
+# Dev environment
+########################################
+
+# Layout
+########################################
+
+# Flashes
+########################################
